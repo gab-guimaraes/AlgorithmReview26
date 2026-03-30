909c00fa91650c9c72db210c22a46d7ce6f6e9ad
gab-guimaraes <dedito182@gmail.com>
2026-03-30 13:44:49 -0300

    implementing searchbyalive

--- src/tree/harrypotter/HarryPotterTree.java
+++ src/tree/harrypotter/HarryPotterTree.java
@@ -1,8 +1,6 @@
 package tree.harrypotter;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Optional;
+import java.util.*;
 
 public class HarryPotterTree {
     private void printBruxos(Tree node) {
@@ -31,22 +29,66 @@ public class HarryPotterTree {
         return Optional.empty();
     }
 
+    public Optional<ArrayList<Person>> searchByAlive(Tree tree) {
+        System.out.println("Searching by alive persons...");
+        if (tree == null)
+            return Optional.empty();
+        ArrayList<Person> alivePersons = new ArrayList<>();
+        Deque<Tree> queue = new ArrayDeque<>();
+        queue.add(tree);
+        while (!queue.isEmpty()) {
+            Tree treePerson = queue.pop();
+            Person popPerson = treePerson.getPerson();
+            if (!popPerson.isDead()) {
+                alivePersons.add(popPerson);
+            }
+            if (treePerson.getLeft() != null) queue.add(treePerson.getLeft());
+            if (treePerson.getRight() != null) queue.add(treePerson.getRight());
+        }
+        System.out.println("Alive people are: "+ alivePersons);
+        return Optional.of(alivePersons);
+    }
+
+    //Altura da arvore
+    //Contar nos por nivel
+    //Caminho ate o bruxo
+    //Menor idade da arvore
+    //usando bfs, primeiro bruxo morto
+    //converter para lista
+
     public static void main(String[] args) {
         HarryPotterTree harryPotterTree = new HarryPotterTree();
+
+        // Avós
+        Person avo1 = new Person("Fleamont Potter", HogwartsHouse.GRIFNORIA, 70, true);
+        Person avo2 = new Person("Euphemia Potter", HogwartsHouse.GRIFNORIA, 68, true);
+
+        // Pais
         Person lilian = new Person("Lilian Potter", HogwartsHouse.GRIFNORIA, 30, true);
         Person thiago = new Person("Thiago Potter", HogwartsHouse.GRIFNORIA, 35, true);
-        Person harry = new Person("Harry Potter", HogwartsHouse.GRIFNORIA, 13, false);
-        Tree harryTree = new Tree(harry);
-        harryTree.setRight(new Tree(thiago));
-        harryTree.setLeft(new Tree(lilian));
-        harryPotterTree.printBruxos(harryTree);
 
-        Optional<Tree> searchNode = harryPotterTree.searchByName(harryTree, "Thiago Potter");
-        if (searchNode.isPresent()){
-            System.out.println(searchNode.get().getPerson().toString());
-        } else {
-            System.out.println("Não Encontrei");
-        }
+        // Harry
+        Person harry = new Person("Harry Potter", HogwartsHouse.GRIFNORIA, 13, false);
+
+        // Root (Harry)
+        Tree harryTree = new Tree(harry);
+
+        // Pais (nível 1)
+        Tree paiNode = new Tree(thiago);
+        Tree maeNode = new Tree(lilian);
+
+        harryTree.setRight(paiNode);
+        harryTree.setLeft(maeNode);
+
+        // Avós (nível 2 - abaixo dos pais)
+        paiNode.setLeft(new Tree(avo1));
+        paiNode.setRight(new Tree(avo2));
+
+        // (Opcional) avós maternos poderiam ir aqui, mas não temos nomes no seu exemplo
+
+        // Execução
+        harryPotterTree.printBruxos(harryTree);
+        harryPotterTree.searchByAlive(harryTree);
     }
 }
 
