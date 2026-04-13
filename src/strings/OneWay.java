cfbecb0ea801ffdd2e9647162d651e4abc87e960
gab-guimaraes <dedito182@gmail.com>
2026-04-12 22:11:12 -0300

    one way

--- src/strings/OneWay.java
+++ src/strings/OneWay.java
@@ -4,13 +4,44 @@ public class OneWay {
     public static boolean oneWayString(String s, String t) {
         if (s == null || t == null) return false;
         if (Math.abs(s.length() - t.length()) > 1) return false;
+        if (s.length() == t.length())
+            return compareReplace(s,t);
+        if (s.length() > t.length())
+            return compareInsert(s, t);
+        else
+            return compareInsert(t, s);
+    }
+
+    public static boolean compareReplace(String s, String t) {
         int divergences = 0;
-        for (int i = 0; i < s.length() - 1; i++) {
-            char sChar = s.charAt(i);
-            char tChar = t.charAt(i);
-            if (sChar != tChar) divergences++;
+        for (int i = 0; i < s.length();i++) {
+            if (s.charAt(i) != t.charAt(i)) {
+                divergences++;
+                if (divergences > 1) return false;
+            }
         }
-        return !(divergences >= 2);
+        return true;
+    }
+
+    //s sempre maior
+    public static boolean compareInsert(String s, String t) {
+        int divergences = 0;
+        int sPointer = 0;
+        int tPointer = 0;
+        //p a l e - s
+        //p l e   - t
+        while (sPointer < s.length() && tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+                tPointer++;
+            } else {
+                sPointer++;
+                divergences++;
+
+                if (divergences > 1) return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
